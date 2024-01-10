07c45f3e5b46a44fd8d7c0dd5c7a8713c5ca675a
Eze <choqooz@gmail.com>
2024-01-10 01:37:55 -0300

    MercadoLiebre

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("node:path");
+
+const app = express();
+const port = 8000;
+
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
